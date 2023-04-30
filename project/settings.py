82e0082b21e353e846e93b7f1d8b49e5821826de
mohamed.ayman92 <momoo@gmail.com>
2023-04-30 17:44:45 +0300

    Deploying To Railway

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'base',
+    'userauths',
+    'taggit', 
+    'crispy_forms',
+    'django_social_share',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'project/static' )
